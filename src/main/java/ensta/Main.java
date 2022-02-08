2460514b1322dd84c11f72060a6c848a6ce138bc
MathieuClave <mathieu.clave@ensta-paris.fr>
2022-02-08 12:19:12 +0100

    fin de tp1

--- src/main/java/ensta/Main.java
+++ src/main/java/ensta/Main.java
@@ -1,11 +1,13 @@
 package ensta;
 
 import ensta.controller.Game;
+import ensta.model.Board;
 
 public class Main {
 
 	public static void main(String args[]) {
-        new Game().init().run();
+        Board board = new Board ("test board", 12);
+        board.print();
     }
 
 }
--- src/main/java/ensta/model/Board.java
+++ src/main/java/ensta/model/Board.java
@@ -1,5 +1,7 @@
 package ensta.model;
 
+import java.util.Iterator;
+
 import ensta.model.ship.AbstractShip;
 import ensta.util.Orientation;
 
@@ -7,16 +9,53 @@ public class Board implements IBoard {
 
 	private static final int DEFAULT_SIZE = 10;
 	private String name;
+	private int size;
 	private char[][] flotte;
 	private boolean[][] frappes;
 	
+	
 	public Board(String nom, int taille) {
 		this.name = nom;
-		this.flotte = new char[taille][taille] temp;
-		this.frappes = new boolean[taille][taille] tempo;
+		this.size = taille;
+		this.flotte = new char[taille][taille];
+		this.frappes = new boolean[taille][taille];
 	}
-
+	
+	public Board(String nom) {
+		this(nom, DEFAULT_SIZE);
+	}
+	
 	public void print() {
+		System.out.println("Navires :                       Frappes :"); 
+		
+		System.out.print("   ");
+		for(int j=1; j<size+1; j++) {System.out.print(String.valueOf((char)(j + 'A' - 1)) + "  ");}
+		System.out.print("     ");
+		for(int j=1; j<size+1; j++) {System.out.print(String.valueOf((char)(j + 'A' - 1)) + "  ");}
+		System.out.print("\n");
+		
+		for (int i=1; i<size+1; i++) {
+			System.out.print(i + " ");
+			if (i<10) {System.out.print(" ");}
+			char affichage_case ;
+			for(int j=1; j<size+1; j++) {
+				if (flotte[i-1][j-1] =='D' || flotte[i-1][j-1] == 'S' ||flotte[i-1][j-1] == 'B' ||flotte[i-1][j-1] == 'C'){
+						affichage_case = flotte[i-1][j-1];
+				}
+				else { affichage_case = '.';}
+				System.out.print(affichage_case + "  ");
+			}
+			System.out.print("  " + i + " ");
+			if (i<10) {System.out.print(" ");}
+			for(int j=1; j<size+1; j++) {
+				if (frappes[i-1][j-1]){
+					affichage_case = 'x';
+				}
+				else { affichage_case = '.';}
+				System.out.print(affichage_case + "  ");
+			}
+			System.out.print(System.lineSeparator());
+		}
 	}
 
 	public boolean canPutShip(AbstractShip ship, Coords coords) {
@@ -55,5 +94,41 @@ public class Board implements IBoard {
 		}
 
 		return true;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean putShip(AbstractShip ship, Coords coords) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasShip(Coords coords) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setHit(boolean hit, Coords coords) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Boolean getHit(Coords coords) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Hit sendHit(Coords res) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
